9f3a7343d29005915b703157910a0f09ce7eac0c
Christie Hill <c@chilldsgn.com>
2024-02-04 19:57:18 +0200

    Strengthen password validation and enhance user features
    This commit strengthens the password validation by enforcing a minimum length, mixed case, numbers, special characters, and checking against known compromised passwords. It also enables terms, privacy policy, and profile photos features. Additionally, it necessitates user email verification before letting them access the app.

--- config/jetstream.php
+++ config/jetstream.php
@@ -58,10 +58,8 @@ return [
     */
 
     'features' => [
-        // Features::termsAndPrivacyPolicy(),
-        // Features::profilePhotos(),
-        // Features::api(),
-        // Features::teams(['invitations' => true]),
+        Features::termsAndPrivacyPolicy(),
+        Features::profilePhotos(),
         Features::accountDeletion(),
     ],
 
