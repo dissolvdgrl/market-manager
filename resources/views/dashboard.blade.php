dd10316ee8d9291d1572bb163cd46b7955350054
Christie Hill <c@chilldsgn.com>
2024-02-11 16:40:58 +0200

    Update welcome message and add conditional view rendering in dashboard
    The welcome message in the dashboard has been updated to be specific for the Banting Market dashboard. Additional links and instructions for application have also been included. Moreover, a condition has been added to the dashboard view to only show the welcome component to users with the pre_approved role.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-
+                @if(Auth::user()->role === 1 )
+                    <x-welcome></x-welcome>
+                @endif
             </div>
         </div>
     </div>
