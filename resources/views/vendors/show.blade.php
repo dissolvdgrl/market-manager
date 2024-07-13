3e8cd6f8dd81b9bdf0cf858940b7bc73699f00a5
dissolvdgrl <c@chilldsgn.com>
2024-07-13 16:01:20 +0200

    Add a link to view an application's details in a vendor's detail page

--- resources/views/vendors/show.blade.php
+++ resources/views/vendors/show.blade.php
@@ -20,11 +20,20 @@
                     </div>
                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                         <dt class="text-sm font-medium leading-6 text-gray-900">Role </dt>
-                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $user->role->name->value }}</dd>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ ucfirst($user->role->name->value) }}</dd>
                     </div>
                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                         <dt class="text-sm font-medium leading-6 text-gray-900">Application details</dt>
-                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">coming soon</dd>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            <p>{{ $user->application->business_name }}</p>
+                            <a href="{{ route('apply.show', ['id' => $user->application->id]) }}" class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
+                                View details
+
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200">
+                                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                        </dd>
                     </div>
                 </dl>
             </div>
