1af6ad9891bcb6654ded9b3d7220ee22d46239aa
dissolvdgrl <c@chilldsgn.com>
2024-07-31 20:51:17 +0200

    WIP - Stand Create Form Livewire component

---
+++ app/Livewire/StandCreateForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class StandCreateForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.stand-create-form');
+    }
+}
---
+++ resources/views/livewire/stand-create-form.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-4">
+    <h2 class="text-xl font-semibold text-gray-900 dark:text-white mb-8">Add a stand</h2>
+    <pre>form goes here</pre>
+</div>
